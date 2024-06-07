5785958dace170925d22f27ab1e36bedd319481c
skrptz <dosomethingwild012712@gmail.com>
2024-06-07 00:36:03 -0600

    z

--- App.js
+++ App.js
@@ -2,22 +2,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
+
 export default function App() {
     const Stack = createNativeStackNavigator();
 
     return (
-        <NavigationContainer style={styles.container}>
-            <Stack.Navigator name="Home" component={HomeScreen} />
-            <Stack.Navigator name="About" component={AboutScreen} />
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="About" component={AboutScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
